badd13957c37f2ae5c34d9931c588ff2112ebe53
ehs2803 <hr2803@naver.com>
2021-09-29 09:59:42 +0900

    20210929

---
+++ 06_Fragment/Fragment/app/src/main/java/com/example/fragment/MenuFragment.java
@@ -0,0 +1,32 @@
+package com.example.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class MenuFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_menu, container, false);
+
+        Button button = rootView.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity activity = (MainActivity) getContext();
+                activity.onFragmentChanged(1);
+            }
+        });
+        // Inflate the layout for this fragment
+        return rootView;
+        //return inflater.inflate(R.layout.fragment_menu, container, false);
+    }
+}
