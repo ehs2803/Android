8a39bf0d8245f29669948f3d804a4d78ee40f0d5
ehs2803 <hr2803@naver.com>
2021-10-22 07:55:10 +0900

    20211022

---
+++ 10_Thread/Thread/app/src/main/java/com/example/thread/MainActivity.java
@@ -0,0 +1,135 @@
+package com.example.thread;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    int value = 0;
+
+    TextView textView, textView2, textView3;
+    Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        textView2 = findViewById(R.id.textView2);
+        textView3 = findViewById(R.id.textView3);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BackgroundThread thread = new BackgroundThread();
+                thread.start();
+            }
+        });
+
+        Button button1 = findViewById(R.id.button2);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BackgroundThread2 thread = new BackgroundThread2();
+                thread.start();
+            }
+        });
+
+        Button button2 = findViewById(R.id.button3);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BackgroundThread3 thread = new BackgroundThread3();
+                thread.start();
+            }
+        });
+
+        handler = new MainHandler();
+    }
+
+    class BackgroundThread extends Thread {
+        int value = 0;
+
+        public void run() {
+            for (int i = 0; i < 100; i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                }
+
+                value += 1;
+                Log.d("Thread", "value : " + value);
+
+                handler.post(new Runnable() {
+                    public void run() {
+                        textView.setText("value 값 : " + value);
+                    }
+                });
+            }
+        }
+    }
+
+    class BackgroundThread2 extends Thread {
+        int value = 0;
+
+        public void run() {
+            for (int i = 0; i < 100; i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                }
+
+                value += 1;
+                Log.d("Thread", "value : " + value);
+
+                handler.post(new Runnable() {
+                    public void run() {
+                        textView2.setText("value 값 : " + value);
+                    }
+                });
+            }
+        }
+    }
+
+    class BackgroundThread3 extends Thread {
+        int value = 0;
+
+        public void run() {
+            for (int i = 0; i < 100; i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch(Exception e) {}
+
+                value += 1;
+                Log.d("Thread", "value : " + value);
+
+                Message message = handler.obtainMessage();
+                Bundle bundle = new Bundle();
+                bundle.putInt("value", value);
+                message.setData(bundle);
+
+                handler.sendMessage(message);
+            }
+        }
+    }
+
+    class MainHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            Bundle bundle = msg.getData();
+            int value = bundle.getInt("value");
+            textView3.setText("value 값 : " + value);
+        }
+    }
+
+}
